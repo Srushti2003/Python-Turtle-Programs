d7c90bc60ca8ff3871ba2454276c3b570a1b85bd
SRUSHTI SUNIL DEWALEKAR <95490034+Srushti2003@users.noreply.github.com>
2022-01-05 17:11:43 +0530

    Create Concentric Circles.py

---
+++ Concentric Circles.py
@@ -0,0 +1,13 @@
+import turtle 
+t = turtle.Turtle()
+r = 10
+for i in range(50):
+    t.circle(r*i)
+    t.up()
+    t.sety((r*i)*(-1))
+    t.down()'''
+'''import turtle
+t = turtle.Turtle()
+for i in range(200):
+    t.right(1)
+    t.forward(1)
