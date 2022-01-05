18a59d2b75a846c9f1642fd6b4ecb15c974aee06
SRUSHTI SUNIL DEWALEKAR <95490034+Srushti2003@users.noreply.github.com>
2022-01-05 17:03:54 +0530

    Delete First Code.py

--- First Code.py
+++
@@ -1,8 +0,0 @@
-# My-First-Repository
-num = int(input("Enter a number: "))
-if num%2 == 0:
-  print("Number entered is even")
-else:
-  print("Number entered is odd")
-  
-print("HI, I'm a student")
