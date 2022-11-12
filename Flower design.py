03e9d8cf02e78350b4f4a94eddcb1f05314793e7
SRUSHTI SUNIL DEWALEKAR <95490034+Srushti2003@users.noreply.github.com>
2022-11-12 14:37:21 +0530

    Create Flower design.py

---
+++ Flower design.py
@@ -0,0 +1,144 @@
+import turtle
+t = turtle.Turtle()
+turtle.Screen().bgcolor("black")
+t.speed(100)
+t.width(2)
+
+t.pencolor("red")
+for i in range(1,32):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,32):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,32):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,32):
+    t.circle(i*5)
+t.right(45)
+
+
+for i in range(1,32):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,32):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,32):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,32):
+    t.circle(i*5)
+t.right(135)
+
+t.pencolor("white")
+for i in range(1,31):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,31):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,31):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,31):
+    t.circle(i*5)
+t.right(45)
+
+for i in range(1,31):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,31):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,31):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,31):
+    t.circle(i*5)
+t.right(135)
+
+t.pencolor("red")
+for i in range(1,21):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,21):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,21):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,21):
+    t.circle(i*5)
+t.right(45)
+
+
+for i in range(1,21):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,21):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,21):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,21):
+    t.circle(i*5)
+t.right(135)
+
+t.pencolor("white")
+for i in range(1,11):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,11):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,11):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,11):
+    t.circle(i*5)
+t.right(45)
+
+
+for i in range(1,11):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,11):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,11):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,11):
+    t.circle(i*5)
+t.right(135)
+
+t.pencolor("red")
+for i in range(1,6):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,6):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,6):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,6):
+    t.circle(i*5)
+t.right(45)
+
+
+for i in range(1,6):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,6):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,6):
+    t.circle(i*5)
+t.right(90)
+for i in range(1,6):
+    t.circle(i*5)
+t.right(135)
